a77d9c808d0ace5fc3a5c99aa448753c1856788e
Benjamin Nwaneampeh <bnwaneampeh@gmail.com>
2020-11-20 15:49:13 +0000

    first pass in refactoring

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -17,30 +17,52 @@ export class GildedRose {
     this.items = items;
   }
 
+  isLegendary(item: Item): boolean {
+    return item.name === 'Sulfuras, Hand of Ragnaros';
+  }
+
+  decrementQuality(item: Item) {
+    item.quality = item.quality === 0 ? 0 : item.quality - 1;
+  }
+
+  incrementQuality(item: Item) {
+    item.quality = item.quality + 1;
+  }
+
+  withinQualityLimit(item: Item): boolean {
+    return item.quality >= 0 && item.quality < 50;
+  }
+
+  isHighQuality(item: Item): boolean {
+    return item.quality < 50;
+  }
+
+  isNotBackstagePass(item: Item): boolean {
+    return (
+      item.name != 'Aged Brie' &&
+      item.name != 'Backstage passes to a TAFKAL80ETC concert'
+    );
+  }
+
+  isTicket(item: Item): boolean {
+    return item.name === 'Backstage passes to a TAFKAL80ETC concert';
+  }
+
   updateQuality() {
     for (const item of this.items) {
-      if (
-        item.name != 'Aged Brie' &&
-        item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      ) {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1;
-          }
+      if (this.isNotBackstagePass(item)) {
+        if (this.withinQualityLimit(item) && !this.isLegendary(item)) {
+          this.decrementQuality(item);
         }
       } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
+        if (this.withinQualityLimit(item)) {
+          this.incrementQuality(item);
+          if (this.isTicket(item)) {
+            if (item.sellIn < 11 && this.isHighQuality(item)) {
+              this.incrementQuality(item);
             }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
+            if (item.sellIn < 6 && this.isHighQuality(item)) {
+              this.incrementQuality(item);
             }
           }
         }
